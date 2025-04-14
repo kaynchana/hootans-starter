ff0f32948a3311451758f57f3b4d1533a4d0ff27
Kayn Chana <iam.kaynchana@gmail.com>
2025-04-14 22:59:50 +0700

    chore: fix cmd biome lint, biome format ignore generator router, change theme

--- tools/playwright-config/src/index.ts
+++ tools/playwright-config/src/index.ts
@@ -1,5 +1,5 @@
-import type { PlaywrightTestConfig } from "@playwright/test";
-import { defineConfig, devices } from "@playwright/test";
+import type { PlaywrightTestConfig } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test';
 
 /**
  * Creates a base Playwright configuration that can be extended by apps
@@ -8,7 +8,7 @@ export default ({ port }: { port: number }): PlaywrightTestConfig => {
   const baseURL = `http://localhost:${port}`;
 
   const config: PlaywrightTestConfig = {
-    testDir: "./tests/e2e",
+    testDir: './tests/e2e',
     /* Run tests in files in parallel */
     fullyParallel: true,
     /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -20,13 +20,13 @@ export default ({ port }: { port: number }): PlaywrightTestConfig => {
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: process.env.CI
       ? [
-          ["list", { printSteps: true }],
-          ["json", { outputFile: "playwright-results.json" }],
-          ["html", { outputFolder: "playwright-report/" }],
+          ['list', { printSteps: true }],
+          ['json', { outputFile: 'playwright-results.json' }],
+          ['html', { outputFolder: 'playwright-report/' }],
         ]
       : [
-          ["list", { printSteps: true }],
-          ["html", { outputFolder: "playwright-report/" }],
+          ['list', { printSteps: true }],
+          ['html', { outputFolder: 'playwright-report/' }],
         ],
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
@@ -34,45 +34,45 @@ export default ({ port }: { port: number }): PlaywrightTestConfig => {
       baseURL: process.env.BASE_URL ?? baseURL,
 
       /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-      trace: "on-first-retry",
+      trace: 'on-first-retry',
     },
 
     /* Configure projects for major browsers */
     projects: [
       // Setup project
-      { name: "setup", testMatch: /.*\.setup\.ts/ },
+      { name: 'setup', testMatch: /.*\.setup\.ts/ },
       {
-        name: "chromium",
-        use: { ...devices["Desktop Chrome"] },
-        dependencies: ["setup"],
+        name: 'chromium',
+        use: { ...devices['Desktop Chrome'] },
+        dependencies: ['setup'],
       },
 
       {
-        name: "firefox",
-        use: { ...devices["Desktop Firefox"] },
-        dependencies: ["setup"],
+        name: 'firefox',
+        use: { ...devices['Desktop Firefox'] },
+        dependencies: ['setup'],
       },
 
       {
-        name: "webkit",
-        use: { ...devices["Desktop Safari"] },
-        dependencies: ["setup"],
+        name: 'webkit',
+        use: { ...devices['Desktop Safari'] },
+        dependencies: ['setup'],
       },
 
       /* Test against mobile viewports. */
       {
-        name: "Mobile Chrome",
-        use: { ...devices["Pixel 5"] },
+        name: 'Mobile Chrome',
+        use: { ...devices['Pixel 5'] },
       },
       {
-        name: "Mobile Safari",
-        use: { ...devices["iPhone 12"] },
+        name: 'Mobile Safari',
+        use: { ...devices['iPhone 12'] },
       },
     ],
 
     /* Run your local dev server before starting the tests */
     webServer: {
-      command: "cd ../../ && pnpm dev",
+      command: 'cd ../../ && pnpm dev',
       url: baseURL,
       reuseExistingServer: !process.env.CI,
     },
